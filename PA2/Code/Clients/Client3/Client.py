7559a0f1c9056300c373cf01e284e5e35f0b96a4
root <root@localhost.localdomain>
2017-10-11 03:51:46 -0500

    Work done for Client3, needs testing then propagation

---
+++ PA2/Code/Clients/Client3/Client.py
@@ -0,0 +1,198 @@
+import socket
+import threading
+import datetime
+import sys
+import time
+from multiprocessing import Process
+from os import walk, path
+import random
+
+
+class Client:
+    def __init__(self):
+        print("Client initialized")
+
+    def testAverageReqTime(self, servers, N):
+        start = datetime.datetime.now()
+        for i in range(N):
+            #sock = self.socketConnect(ip, int(self.selectServer(servers)))
+            filename = "c"+str(i % 8)+"-1"
+            self.decentralizedLookup(filename, servers)
+        end = datetime.datetime.now()
+        delta = end - start
+        return (delta.total_seconds())/N
+
+    def testMulti(self, servers, N):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(("127.0.1.1", 12301))  # Defines peer's port for test
+        sock.listen()
+        print("\nClient ready and awaiting start signal from server\n")
+        client, clientip = sock.accept()
+        time.sleep(0.5)
+        print("Signal received, starting test\n")
+        threading._start_new_thread(self.testAverageReqTime, (servers, N))
+        #result = self.testAverageReqTime(servers, N)
+        print("\n[The average request took " + str(result) + " seconds]\n")
+
+    def selectServer(self, servers):
+        #This is where we'd normally select based on performance
+        return random.choice(servers)
+
+    def socketConnect(self, ip, port):
+        try:
+            so = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            so.connect((ip, port))
+            print("Connected to " + str(ip) + " on port " + str(port) + "\n")
+            return so
+        except:
+            print("Couldn't connect to " + str(ip))
+
+    def register(self, filename, sock, port):
+        sock.send(("register " + filename + " " + str(port)).encode())
+
+    def findAllFiles(self):
+        files = []
+        for (dirpath, dirnames, filenames) in walk("."):  # parse all directories and files
+            files.extend(filenames)
+            break   # break after one iteration to remain at the same level
+        if "Client.py" in files:
+            files.remove("Client.py")
+        if "Server.py" in files:
+            files.remove("Server.py")
+        return files
+
+    def lookup(self, filename, sock, port):
+        sock.send(("lookup " + filename + " " + str(port)).encode())
+        return sock.recv(4096).decode()
+        #print(sock.recv(4096).decode())
+
+    def decentralizedLookup(self, filename, servers):
+        result = ""
+        for indexport in servers:
+            sock = client.socketConnect(indexip, int(indexport))
+            if sock:
+                result = client.lookup(filename, sock, server.port)
+                # print(sock.recv(4096).decode())
+                sock.shutdown(socket.SHUT_WR)
+                if "404 - No peer has registered this file" not in result:  # keeps the decentralization transparent by only showing one result message
+                    print(result)
+                    break
+        if "404 - No peer has registered this file" in result:
+            print(result)
+
+    def getFile(self, filename, sock):
+        tempfilename = filename
+        while path.isfile(tempfilename):
+            tempfilename += "_"
+        f = open(tempfilename, 'w')
+        sock.send(("get " + str(filename)).encode())
+        print("Receiving file data")
+        fdata = sock.recv(4096).decode()
+        while (fdata):
+            print("Receiving file data")
+            f.write(fdata)
+            fdata = sock.recv(4096).decode()
+        f.close()
+        print("\nDisplay file \'" + filename + "\'\n")
+        print(sock.recv(4096).decode())
+
+
+class Server:
+    def __init__(self):
+        self.host = socket.gethostbyname(socket.gethostname())
+        self.port = 12343
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+
+    def giveFile(self, filename, sock):
+        f = open(filename, 'r')
+        fdata = f.read(4096)
+        while (fdata):
+            sock.send(fdata.encode())
+            fdata = f.read(4096)
+        f.close()
+        sock.shutdown(socket.SHUT_WR)
+
+    def threadedListening(self):
+        self.sock.listen(5)  # Limit to 5 concurrent connections
+        lock = threading.Lock()
+        lock.acquire()
+        print("\n[LISTENING PROCESS]\nServer socket listening...\n")
+        lock.release()
+        while 1:
+            client, ip = self.sock.accept()
+            print("Connection received from "+str(client.getsockname()[0])+" "+str(client.getsockname()[1]))
+            client.settimeout(120)  # Terminate after 2min of inactivity
+            threading._start_new_thread(self.Listen, (client, ip, lock))
+
+    def Listen(self, client, ip, lock):
+        data = client.recv(4096).decode()
+        infos = data.split(" ")
+        if infos[0] == "get":
+            lock.acquire()
+            self.giveFile(infos[1], client)
+            lock.release()
+
+
+if __name__ == "__main__":
+
+    cfg = open("../../Indexcfg", "r")
+    servers = cfg.read().strip().split("|")
+    cfg.close()
+    server = Server()
+    client = Client()
+    listenerProcess = Process(target=server.threadedListening).start()
+    indexip = "127.0.1.1"
+    # indexport = input("Indexing server port ?\n")
+    while True:
+        userinput = input(
+            "Enter commmand:\n"
+            " \"get\" to download a file.\n"
+            " \"register\" to index one of your files\n"
+            " \"lookup\" to query for a desired file location\n"
+            " \"test\" to enter the performance testing mode\n"
+            " \"exit\" to quit the program\n"
+            "or let the program run for the server to listen\n")
+        if userinput == "get":
+            userinput = input("Filename ?\n")
+            # ip = input("File host IP ?\n")
+            ip = "127.0.1.1"
+            port = input("File host port ?\n")
+            sock = client.socketConnect(ip, int(port))
+            if sock:
+                client.getFile(userinput, sock)
+                print(sock.recv(4096).decode())
+                sock.shutdown(socket.SHUT_WR)
+        elif userinput == 'register':
+            files = client.findAllFiles()
+            indexport = client.selectServer(servers)
+            sock = client.socketConnect(indexip, int(indexport))
+            if sock:
+                for f in files:
+                    f.replace(" ", "")
+                    client.register(f, sock, server.port)
+                    print(sock.recv(4096).decode())
+                    sock = client.socketConnect(indexip, int(indexport))
+                sock.shutdown(socket.SHUT_WR)
+        elif userinput == 'lookup':
+            filename = input("Filename ?\n").replace(" ", "")
+            client.decentralizedLookup(filename, servers)
+        elif userinput == "exit":
+            print("warning, this will also terminate the server process\n")
+            input = input("Are you sure ? (y/n)\n")
+            if input == "y":
+                sys.exit()
+        elif userinput == "test":
+            N = input("How many requests ?\n")
+            response = input("Multithreading test ? (y/n)\n")
+            if response == "n":
+                print("Beginning of test :\n")
+                result = client.testAverageReqTime(servers, int(N))
+                print("[The average request took " + str(result) + " seconds]")
+            if response == "y":
+                client.testMulti(servers, int(N))  # Gotta change that shit
+
+        else:
+            print("Incorrect command.\n")
---
+++ PA2/Code/Servers/Server5/Server.py
@@ -0,0 +1,91 @@
+import socket
+import threading
+from multiprocessing import Process, Pool
+from collections import defaultdict
+
+class Server:
+
+    def __init__(self):
+        self.files = defaultdict(list)  #Data structure : dictionnary of lists
+        self.host = socket.gethostbyname(socket.gethostname())
+        self.port = 12005
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+
+    def fireSignals(self):
+        pool = Pool(4)
+        ports = [12346]
+        #ports = [12346, 12347, 12348, 12349] #Change this according to how many concurrent clients you want
+        pool.map(self.signal, ports)
+        pool.close()
+        pool.join()
+
+    def signal(self, port):
+        #send test start signal to awaiting client
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        if sock:
+            sock.connect(("127.0.1.1", port))
+            sock.shutdown(socket.SHUT_WR)
+
+    def register(self, filename, hostip):
+        if hostip in self.files[filename]:
+            return ""   #File was already added so nothing happens
+        else:
+            self.files[filename].append(hostip)
+            return filename + " successfully added"
+
+    def search(self, filename):
+        if filename in self.files:
+            portstring = ""
+            for each in self.files[filename]:
+                portstring += str(each) + " "
+            if len(self.files[filename]) == 1:
+                return "Client on port " + portstring.strip() + " has this file"
+            else:
+
+                return "Clients on ports " + portstring.strip() + " have this file"
+        else:
+            return "404 - No peer has registered this file"
+
+    def threadedListening(self):
+        self.sock.listen()  # Limit to 5 concurrent connections
+        print("Server socket listening on port "+str(self.port)+"...\n")
+        while 1:
+            client, ip = self.sock.accept() #Program waits here for a call from a client
+            print("\nConnection received from " + str(client.getsockname()[0]))
+            client.settimeout(120)  # Terminate after 2min of inactivity
+            threading._start_new_thread(self.Listen, (client, ip))
+
+
+    def Listen(self, client, ip):
+        data = client.recv(4096).decode()
+        if data:
+            infos = data.split(" ")
+            if infos[0] == "lookup":
+                result = self.search(infos[1])
+                client.send(result.encode())
+                client.shutdown(socket.SHUT_WR)
+                print("\nCurrent file list :")
+                print(str(self.files))
+            elif infos[0] == "register":
+                result = self.register(infos[1], infos[2])
+                client.send(result.encode())
+                client.shutdown(socket.SHUT_WR)
+                print("\nCurrent file list :")
+                print(str(self.files))
+            else:
+                client.send("Unrecognized command".encode())
+
+
+if __name__ == "__main__":
+    server = Server()
+    print("Local IP is : " + server.host)
+    while 1:
+        result = input("\nInput START to boot the server or "
+                       "TEST to fire test signals to the clients\n")
+        if result == "START":
+            server.threadedListening()
+        elif result == "TEST":
+            server.fireSignals()
+            server.threadedListening()
